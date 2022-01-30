c54f17fa28ef21dc727620f98a2c1dcff7b35bdb
Ahmedaouladaomar <ahmedaouladomar@gmailcom>
2022-01-30 01:10:49 +0100

    make it better

---
+++ src/pages/elements/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+
+   
+
+  return <div className='navbar-container'>
+              <a className='home-links' href=''>Home</a>
+              <a className='home-links' href=''>Projects</a>
+              <a className='home-links' href=''>About me</a>
+              <a className='home-links' href=''>Contact</a>
+         </div>;
+}
+
+
+
+export default Navbar;
